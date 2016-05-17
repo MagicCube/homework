2f72962551a711e24b7f5d2a634e10007716afbd
Jason121250148 <962215093@qq.com>
2016-05-17 10:50:06 +0800

    Modify index.js.

---
+++ 05-map-reduce/index.js
@@ -0,0 +1,17 @@
+const arr = [ null, null, 1, 3, null, 2, null, 1, null, null, 1, null, null ];
+
+function interpo(arr)
+{
+    const first = arr.find(item => item !== null);
+    arr.reduce((pre, cur, index) => {
+        if(cur === null){
+            arr[index] = pre;
+        }
+        return arr[index];
+    }, first);
+    // arr.reduce((pre, cur, index) => {cur === null ? arr[index] = pre : 0;return arr[index];},arr.find(item => item != null));
+}
+
+console.log(arr);
+interpo(arr);
+console.log(arr);
