76bae9ed7878f5980af36fbe70e0bb74da68878b
LucasYuNju <LucasYuNju@gmail.com>
2016-05-17 10:50:27 +0800

    Final version.

--- 05-map-reduce/index.js
+++ 05-map-reduce/index.js
@@ -1,7 +1,19 @@
 const arr = [ null, null, 1, 3, null, 2, null, 1, null, null, 1, null, null];
 function interpos(arr)
 {
-    arr.reduce((prev, cur, idx) => cur === null ? arr[idx] = arr[idx - 1] : null, arr[0] = arr.find(item => item !== null));
+    // one line
+    // arr.reduce((prev, cur, idx) => cur ? cur : arr[idx] = prev, arr[0] = arr.find(item => item !== null));
+    const firstNonNull = arr.find(item => item);
+    arr.reduce((prev, cur, idx) => {
+        if(cur)
+        {
+            return cur;
+        }
+        else
+        {
+            return arr[idx] = prev;
+        }
+    }, firstNonNull);
 }
 console.log(arr);
 interpos(arr);
