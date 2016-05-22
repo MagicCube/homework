32414024cc3ea65ff7c39470fc4799288b0bde2c
huayun liu <huayun.liu@sap.com>
2016-05-22 11:12:59 +0800

    One Line..

--- 05-map-reduce/index.js
+++ 05-map-reduce/index.js
@@ -1,9 +1,6 @@
 const arr = [ null, null, 1, 3, null, 2, null, 1, null, null, 1, null, null ];
 
-function interpo(arr)
-{
-  
-}
+const interpo = arr => arr.reduce((pre, bef, i, array) => array[i] = bef || pre, 1);
 
 console.log(arr);
 interpo(arr);
