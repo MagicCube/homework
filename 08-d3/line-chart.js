5b05b6663ce1099dc048d7e7439d42cb096e601d
LucasYuNju <LucasYuNju@gmail.com>
2016-05-24 22:01:47 +0800

    Draw broken line graph.

---
+++ 08-d3/line-chart.js
@@ -0,0 +1,56 @@
+var margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 20
+}
+var width = 900 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var xScale = d3.scale.linear()
+    .range([0, width])
+    .domain([0, 12]);
+
+var yScale = d3.scale.linear()
+    .range([height, 0])
+    .domain([0, 10]);
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return xScale(d.month); })
+    .y(function(d) { return yScale(d.value); });
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.json("data.json", function(error, data) {
+  if (error) throw error;
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .append("text")
+      .attr("x", width)
+      .attr("dx", "-3em")
+      .text("Month");
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+});
